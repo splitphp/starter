95e1afa7872bda3f6fe50b39ee60aacb447f8944
=Gabriel Valentoni Guelfi <=gabriel.valguelfi@gmail.com>
2025-05-28 21:02:55 -0300

    replaced kernel classes

--- public/index.php
+++ public/index.php
@@ -30,8 +30,8 @@ use \engine\System;
 use \engine\Helpers;
 
 // Includes main class System:
-require_once __DIR__ . "/../engine/class.system.php";
-require_once __DIR__ . "/../engine/class.helpers.php";
+require_once __DIR__ . "/../engine/kernel/class.system.php";
+require_once __DIR__ . "/../engine/kernel/class.helpers.php";
 
 // Initiate the application, running the main class System:
 try {
